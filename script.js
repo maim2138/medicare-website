5f0a637515ff27850e29ed522516de38afb90ed3
Derrick <005.shoot.to.play@gmail.com>
2025-04-01 04:16:36 +0300

    updated script.js

--- script.js
+++ script.js
@@ -126,3 +126,28 @@ document.addEventListener("DOMContentLoaded", () => {
         }
    });
 });
+
+const modal = document.getElementById("signupModal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementsByClassName("close-btn")[0];
+
+openModalBtn.addEventListener("click", function() {
+    modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Registration complete!");
+    modal.style.display = "none";
+});
